7c78048686b99d09001882f618ce1e5813515e9c
Jone <jone.h.d@hotmail.no>
2023-09-11 11:35:15 +0200

    add supply_demand folder

---
+++ projects/salmon_price_prediction/supply_demand/regression.R
@@ -0,0 +1,4 @@
+supply_data <- read.csv(
+  "projects/salmon_price_prediction/supply_demand/supply_data.csv",
+  header = TRUE
+)
--- projects/salmon_price_prediction/utils.py
+++ projects/salmon_price_prediction/utils.py
@@ -1,5 +1,8 @@
 import requests
 import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 def import_salmon_data(URL: str, correct_dt: bool=True, rename_cols: bool=False) -> pd.DataFrame: 
     '''
@@ -38,3 +41,40 @@ def add_lags(df: pd.DataFrame):
     df['Week'] = df.index.isocalendar().week.astype(int)
     df['Month'] = df.index.month
     df['Day of year'] = df.index.dayofyear
+
+
+class xgb_model:
+    def __init__(self, data: pd.DataFrame, targets: list[str], features: list[str]):
+        self.data = data
+        self.targets = targets
+        self.features = features
+
+        # Create model
+        self.reg_model = xgb.XGBRegressor(
+            n_estimators=10000, 
+            early_stopping_rounds=1000
+        )
+
+        # Create train and test sets
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.data[self.features], 
+            self.data[self.targets], 
+            test_size=0.30, 
+            shuffle=True
+        )
+
+        # Fit model
+        self.reg_model.fit(
+            self.X_train, self.y_train, 
+            eval_set=[(self.X_train, self.y_train), (self.X_test, self.y_test)], 
+            verbose=False
+        )
+
+        self.y_pred = self.reg_model.predict(self.X_test)
+
+    def predict(self, X_pred: pd.DataFrame):
+        return self.reg_model.predict(X_pred)
+
+    def mse(self):
+        mse = mean_squared_error(self.y_test, self.y_pred)
+        print(f"Mean Squared Error: {mse:.4f}")
